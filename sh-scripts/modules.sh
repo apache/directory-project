9f29b33111f815431a8204718bc64b760ecef243
Ersin Er <ersiner@apache.org>
2006-02-07 14:40:25 +0000

    Made the script sort the output.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks@375614 13f79535-47bb-0310-9956-ffa450edef68

--- sh-scripts/modules.sh
+++ sh-scripts/modules.sh
@@ -3,4 +3,4 @@
 # This script lists the modules of a project looking
 # at the pom whose name given as a parameter.
 
-xml_grep "/project/modules/module" $1  | grep '<module>.*</module>' | sed 's/.*>\(.*\)<.*/\1/g'
+xml_grep "/project/modules/module" $1  | grep '<module>.*</module>' | sed 's/.*>\(.*\)<.*/\1/g' | sort
