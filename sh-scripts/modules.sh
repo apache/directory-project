d9d5daf121b5375bbb033c1c52eaf84cf41f5622
Ersin Er <ersiner@apache.org>
2006-02-07 20:42:11 +0000

    Added some simple scripts to query project poms.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks@375691 13f79535-47bb-0310-9956-ffa450edef68

--- sh-scripts/modules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This script lists the modules of a project looking
-# at the pom whose name given as a parameter.
-
-xml_grep "/project/modules/module" $1  | grep '<module>.*</module>' | sed 's/.*>\(.*\)<.*/\1/g' | sort
