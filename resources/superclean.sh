6e6641cb044bcb2f05af2ed4dc2ac8cc139c8a99
Ersin Er <ersiner@apache.org>
2006-02-08 21:39:35 +0000

    Fix find command. Fix test command.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks@376084 13f79535-47bb-0310-9956-ffa450edef68

--- resources/superclean.sh
+++ resources/superclean.sh
@@ -3,9 +3,9 @@
 # This script is supposed to be run from 'directory/trunks'
 # with the command 'resources/superclean.sh'
 
-find . -name -type d target | xargs rm -rf
+find . -type d -name target | xargs rm -rf
 
-if [ `uname`="CYGWIN_NT-5.1" ]; then
+if [ `uname` == "CYGWIN_NT-5.1" ]; then
   rm -rf $(cygpath "$HOME_PATH"/.m2/repository/org/apache/directory);
 else
   rm -rf ~/.m2/repository/org/apache/directory/
