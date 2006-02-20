f7cd31a93fc97c1c0aceb733578eef30d706c14c
Ersin Er <ersiner@apache.org>
2006-02-20 11:07:23 +0000

    Updated the script to display current working directory.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks@379082 13f79535-47bb-0310-9956-ffa450edef68

--- resources/codesize.sh
+++ resources/codesize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script is supposed to be run from 'directory/trunks/resources'
-# with the command './codesize.sh'
+# This script is supposed to be run from 'directory/trunks'
+# with the command 'resources/codesize.sh'
 
-echo Total lines of Java code is $(find .. -type f -regex '.*\.java' | xargs cat | wc -l).
+echo Total lines of Java code under $PWD is $(find . -type f -regex '.*\.java' | xargs cat | wc -l).
