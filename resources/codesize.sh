b1e4130b76b5f2879959e1e9a8c7e323c53927cc
Ersin Er <ersiner@apache.org>
2006-02-20 11:00:41 +0000

    Updated codesize script explanation.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks@379079 13f79535-47bb-0310-9956-ffa450edef68

--- resources/codesize.sh
+++ resources/codesize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script is supposed to be run from 'directory/trunks'
-# with the command 'sh-scripts/codesize.sh'
+# This script is supposed to be run from 'directory/trunks/resources'
+# with the command './codesize.sh'
 
 echo Total lines of Java code is $(find .. -type f -regex '.*\.java' | xargs cat | wc -l).
