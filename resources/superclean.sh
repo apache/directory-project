903b6604903a66d5e04e87151213f44dcc9a7837
Ersin Er <ersiner@apache.org>
2006-02-26 02:01:21 +0000

    Ultra clean for org/apache/mina in local m2 repo.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks@381046 13f79535-47bb-0310-9956-ffa450edef68

--- resources/superclean.sh
+++ resources/superclean.sh
@@ -6,8 +6,8 @@
 find . -type d -name target | xargs rm -rf
 
 if [ `uname` == "CYGWIN_NT-5.1" ]; then
-  rm -rf $(cygpath "$HOME_PATH"/.m2/repository/org/apache/directory);
+  rm -rf $(cygpath "$HOME_PATH"/.m2/repository/org/apache/{directory,mina});
 else
-  rm -rf ~/.m2/repository/org/apache/directory/
+  rm -rf ~/.m2/repository/org/apache/{directory,mina}
 fi
 
