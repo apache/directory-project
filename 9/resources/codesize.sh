199cbfbdde29eb781c688a9008122aa1c976cee3
Alex Karasulu <akarasulu@apache.org>
2008-02-12 20:11:28 +0000

    rolling back
    git-svn-id: https://svn.apache.org/repos/asf/directory/project/releases@627074 13f79535-47bb-0310-9956-ffa450edef68

--- 9/resources/codesize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This script is supposed to be run from 'directory/trunks'
-# with the command 'resources/codesize.sh'
-
-echo Total lines of Java code under $PWD is $(find . -type f -regex '.*\.java' | xargs cat | wc -l).
